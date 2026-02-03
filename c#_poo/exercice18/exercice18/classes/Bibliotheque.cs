89a40396b424650797714438c1124fefeed39e24
Frederic <fred@utopios.solutions>
2026-02-03 16:09:40 +0100

    exercice18

---
+++ c#_poo/exercice18/exercice18/classes/Bibliotheque.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Collections.Specialized.BitVector32;
+
+namespace exercice18.classes
+{
+    internal class Bibliotheque
+    {
+         List<Livre> livre = new List<Livre>();
+
+
+
+        public void Ajouter(Livre livre)
+        {
+            this.livre.Add(livre);
+            Console.WriteLine("Livre ajoute !");
+        }
+        public void SupprimerLivre(int numero)
+        {
+            livre.Remove(livre );
+            Console.WriteLine("Livre supprimer !");
+
+        }
+
+
+
+
+       
+
+
+
+
+    }         
---
+++ c#_poo/exercice18/exercice18/classes/Livre.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercice18.classes
+{
+    internal class Livre
+    {
+
+        private int Numero { get; set; }
+        private string Titre { get; set; }
+        private string Auteur { get; set; }
+        private int ExemplairesDisponibles { get; set; }
+
+        public Livre(int numero, string titre, string auteur, int exemplaires)
+        {
+            Numero = numero;
+            Titre = titre;
+            Auteur = auteur;
+            ExemplairesDisponibles = exemplaires;
+        }
+
+        public override string ToString()
+        {
+            return $"numéro: {Numero}, titre: {Titre}, auteur: {Auteur}" +
+                $", exemplaires: {ExemplairesDisponibles}";
+        }
+
+
+
+
+
+    }
+}
