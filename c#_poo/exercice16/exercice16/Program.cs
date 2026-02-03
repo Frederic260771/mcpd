ba49807d24f7064186c6b094bd8fb25a26b5aa68
Frederic <fred@utopios.solutions>
2026-02-03 14:10:24 +0100

    exercice 16

---
+++ c#_poo/exercice16/exercice16/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+List<string> mots = new List<string>();
+string saisie;
+Console.WriteLine("taper des mots ( stop)pour finir  :");
+
+do
+{
+    saisie = Console.ReadLine();
+
+    if (saisie != "stop")
+    {
+        mots.Add(saisie);
+
+    }
+
+}while (saisie != "stop");
+
+
+
+
+Console.WriteLine("Nombre de mots saisis : " + mots.Count);
+Console.WriteLine("Liste des mots :");
+
+foreach (string mot in mots)
+{
+    Console.WriteLine(mot);
+}
+
