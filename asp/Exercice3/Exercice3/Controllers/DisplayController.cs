2bbb3c98cf724a4544302dcf90ae52e16fff5fe3
Frederic <fred@utopios.solutions>
2026-02-17 10:27:45 +0100

    exo3

---
+++ asp/Exercice3/Exercice3/Controllers/DisplayController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercice3.Controllers
+{
+    internal class DisplayController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            string prenom = "fred";
+            int age = 54;
+
+            List<string> hobbies = new List<string>()
+            {
+                "Informatique","Musique"
+            };
+
+            ViewData["Prenom"] = prenom;
+            ViewData["Age"] = age;
+            ViewData["hobbies"] = hobbies;
+
+
+            return View();
+        }
+    }
+}
+
+
+
+
+
+
+
+}
+}
